d377a6ea58e39429dcfb48fea29d67e84e45f62f
AkKiL <78695802+HeimanPictures@users.noreply.github.com>
2023-05-30 09:52:46 +0000

    Process Q1

---
+++ process.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    /**
+    This is the parent process with PID: 12074
+    Child process PID: 12082
+    This is the child process with PID: 12082
+    Parent process PID: 12074
+    README.md  process  process.c
+    Child process exited with status: 0
+    */
+    pid_t pid;
+    int status;
+    pid = fork();
+    if(pid<0) {
+        printf("Error: fork() failed.\n");
+        return 1;
+    } else if (pid==0) {
+        printf("This is the child process with PID: %d\n", getpid());
+        printf("Parent process PID: %d\n", getppid());
+
+        execlp("/bin/ls", "ls", NULL);
+        printf("This shouyld not be printed if exec() is successful.\n");
+        return 0;
+    } else {
+        printf("This is the parent process with PID: %d\n", getpid());
+        printf("Child process PID: %d\n", pid);
+        wait(&status);
+        printf("Child process exited with status: %d\n", status);
+        return 0;
+    }
+}
