ed5b7b1bc27c8d40c6b46cc40436dccc4faa7c70
AkKiL <78695802+HeimanPictures@users.noreply.github.com>
2023-07-25 10:12:30 +0000

    Added FIFO & IRU of Module 4

---
+++ Lab/mod4/fifo.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main() {
+    /*output:
+    Enter the number of pages:7
+    Enter the page number:1 3 0 3 5 6 3
+    Enter the number of frames:3
+            input   page frames
+    1               1       -1      -1
+    3               1       3       -1
+    0               1       3       0
+    3
+    5               5       3       0
+    6               5       6       0
+    3               5       6       3
+    Page Fault is 6
+    */
+    int i,j,n,a[50],frame[10],no,k,avail,fault=0;
+    printf("Enter the number of pages:");
+    scanf("%d",&n);
+    printf("Enter the page number:");
+    for(i=1;i<=n;i++)
+    scanf("%d",&a[i]);
+    printf("Enter the number of frames:");
+    scanf("%d",&no);
+    for(i=0;i<no;i++)
+    frame[i]=-1;
+    j=0;
+    printf("\tinput\tpage frames\n");
+    for(i=1;i<=n;i++) {
+        printf("%d\t\t",a[i]);
+        avail=0;
+        for(k=0;k<no;k++)
+        if(frame[k]==a[i])
+        avail=1;
+        if(avail==0) {
+            frame[j]=a[i];
+            fault++;
+            for(k=0;k<no;k++)
+            printf("%d\t",frame[k]);
+            j=(j+1)%no;
+        }
+        printf("\n");
+    }
+    printf("Page Fault is %d\n",fault);
+    return 0;
+}
---
+++ Lab/mod4/lru.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<limits.h>
+int checkhit(int incomingPage, int queue[], int occupied){
+    for(int i=0;i<occupied;i++){
+        if(incomingPage==queue[i])
+        return 1;
+    }
+    return 0;
+}
+void printFrame(int queue[],int occupied){
+    for(int i=0;i<occupied;i++)
+    printf("%d\t",queue[i]);
+}
+void main(){
+    int incomingStream[]={1,2,3,2,1,5,2,1,6,2,5,6,3,1,3};
+    int n=sizeof(incomingStream)/sizeof(incomingStream[0]);
+    int frames=3,queue[n],distance[n],occupied=0,pagefault=0;
+    printf("Page\tFrame1\tFrame2\tFrame3\n");
+    for(int i=0;i<n;i++){
+        printf("%d:\t",incomingStream[i]);
+        if(checkhit(incomingStream[i],queue,occupied)) {
+            printFrame(queue,occupied);
+        } else if (occupied<frames){
+            queue[occupied]=incomingStream[i];
+            pagefault++;
+            occupied++;
+            printFrame(queue, occupied);
+        } else {
+            int max=INT_MIN,index;
+            for(int j=0;j<frames;j++){
+                distance[j]=0;
+                for(int k=i-1;k>=0;k--){
+                    ++distance[j];
+                    if(queue[j]==incomingStream[k])
+                    break;
+                }
+                if(distance[j]>max){
+                    max=distance[j];
+                    index=j;
+                }
+            }
+            queue[index]=incomingStream[i];
+            printFrame(queue,occupied);
+            pagefault++;
+        }
+        printf("\n");
+    }
+    printf("Page Fault:%d",pagefault);
+}
